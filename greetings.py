d1f16d775710d9138b91736a5fcfbc7b24d6c818
Stephany Pena <stephany.pena@mail.citytech.cuny.edu>
2020-11-02 17:06:12 -0500

    added 6 files

---
+++ greetings.py
@@ -0,0 +1,6 @@
+def greet():
+    print("hello")
+
+
+for i in range(0, 5):
+    greet()
---
+++ pluralize.py
@@ -0,0 +1,7 @@
+def pluralize(word):
+    return word + "s"
+        
+userword = input("Please give a word: ")
+
+print(pluralize(userword))
+
